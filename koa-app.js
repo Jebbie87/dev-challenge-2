3c98e564c9551adf8089829d4e94890423310873
Jebbie87 <jeffreycj.chang@gmail.com>
2017-03-23 19:53:15 -0400

    Added a body that shows some of the saved data

--- koa-app.js
+++ koa-app.js
@@ -1,19 +1,22 @@
+'use strict'
+
 require('dotenv').load()
 const Koa = require('koa')
 const request = require('koa-request')
 const app = new Koa()
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
+app.proxy = true
+
 app.use(function *(next) {
   // const ip = this.request.ip // this should be grabbing the ip address
+  console.log(this.request.ip)
   const ip = '208.80.152.201' // dummy ip data
-
 
   // api call to grab lat and lon
   const geoURL = {
     url: `http://ip-api.com/json/${ip}`,
   }
-
   const geolocation = yield request(geoURL)
   const location = JSON.parse(geolocation.body)
 
@@ -34,6 +37,15 @@ app.use(function *(next) {
   this.humidity = weather.main.humidity
   this.weatherCode = weather.cod
 
+  this.body =
+  `  City name = ${this.name}
+  City temp = ${this.temp}
+  City temp min = ${this.tempMin}
+  City temp max = ${this.tempMax}
+  City weather code = ${this.weatherCode}
+  City longitude = ${this.lon}
+  City latitude = ${this.lat}
+  `
   yield next
 })
 
